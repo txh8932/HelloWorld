c822b9a3aa291a0c066a5330be3d2a4225419621
Ho Thoa <98070554+txh8932@users.noreply.github.com>
2022-01-30 19:34:19 -0500

    Update bonus.java

--- bonus.java
+++ bonus.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Hello {
+public class bonus {
     public static void main (String[] args) {
       Scanner keyboard = new Scanner(System.in);
       System.out.println("What is your name?");
