c60ba790507108e3f68ef5869a2faa016e324064
Ho Thoa <98070554+txh8932@users.noreply.github.com>
2022-01-30 19:38:42 -0500

    Update extreme_bonus.java

--- extreme_bonus.java
+++ extreme_bonus.java
@@ -1,4 +1,4 @@
-public class extremebonus {
+public class extreme_bonus {
   public static void main(String[] args) {
     String name = System.getenv("USER");
     System.out.println("Hello, " + name);
